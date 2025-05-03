3b664704400b0ac7860f9c6f4f9786c1e89ce785
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-05-02 22:53:01 -0700

    add hyprland and kitty config files

--- run.sh
+++ run.sh
@@ -2,11 +2,16 @@
 
 
 # Setup desktop
-sudo pacman -S hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm
+sudo pacman -S --noconfirm hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm ttf-hack-nerd
+
+cp -r ./dotfiles/.config/ ~/.config/
+
 
 # Setup system
 
 # Setup packages
+
+sudo pacman -S --noconfirm ttf-hack-nerd
 
 
 # Setup config files
