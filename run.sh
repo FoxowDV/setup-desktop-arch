ddbaa165e6f1909772239f5d81126eb26a37db26
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-05-02 22:40:05 -0700

    add hyprland install

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# Setup desktop
+sudo pacman -S hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm
+
+# Setup system
+
+# Setup packages
+
+
+# Setup config files
