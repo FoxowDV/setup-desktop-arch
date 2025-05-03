f9c24ee8aa04453ddb4376365d10f09d99d4ac15
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-05-03 11:46:40 -0700

    add yay install

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+# Setup yay
+sudo pacman -S --noconfirm --needed git base-devel
+cd ..
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ../setup-desktop-arch/
+
 # Setup desktop
 sudo pacman -S --noconfirm hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm ttf-hack-nerd fish waybar neovim greetd ttf-hack-nerd
 cp -r ./dotfiles/.config/* ~/.config/
