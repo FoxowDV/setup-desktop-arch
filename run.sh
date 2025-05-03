9a130b15c4b07a14842de97d61531239c04bbc40
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-05-02 23:34:28 -0700

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-
 # Setup desktop
-sudo pacman -S --noconfirm hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm ttf-hack-nerd fish waybar neovim greetd
-
+sudo pacman -S --noconfirm hyprland dunst kitty dolphin rofi xdg-desktop-portal-hyprland qt5-wayland qt6-wayland polkit-kde-agent grim uwsm ttf-hack-nerd fish waybar neovim greetd ttf-hack-nerd
 cp -r ./dotfiles/.config/* ~/.config/
 
+
+## Greeter
 sudo cp -r ./files/greetd/* /etc/greetd/
+sudo systemctl enable greetd.service
 
 
-# Setup system
-
-# Setup packages
-
-sudo pacman -S --noconfirm ttf-hack-nerd
-
-
-# Setup config files
