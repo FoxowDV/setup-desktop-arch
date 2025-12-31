770027e0db6e1d0a63f0c00b18a7d5fb75ca5db7
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-12-31 11:21:52 -0700

    remove yazi

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -91,37 +91,4 @@ return {
     end,
   },
 
--- vim.keymap.set('t', 't', ":<Cmd>execute 'y'<CR>", { desc = "Execute 'y' in terminal mode" })
-	---@type LazySpec
-	{
-	  "mikavilpas/yazi.nvim",
-	  event = "VeryLazy",
-	  keys = {
-		{
-		  "<leader>-",
-		  mode = { "n", "v" },
-		  "<cmd>Yazi<cr>",
-		  desc = "Open yazi at the current file",
-		},
-		{
-		  -- Open in the current working directory
-		  "t",
-		  "<cmd>Yazi cwd<cr>",
-		  desc = "Open the file manager in nvim's working directory",
-		},
-		{
-		  "<c-up>",
-		  "<cmd>Yazi toggle<cr>",
-		  desc = "Resume the last yazi session",
-		},
-	  },
-	  ---@type YaziConfig
-	  opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
-		keymaps = {
-		  show_help = "<f1>",
-		},
-	  },
-	},
 }
--- updateFiles.sh
+++ updateFiles.sh
@@ -10,7 +10,6 @@ cp -r ~/.config/kitty ${path}
 cp -r ~/.config/fastfetch/ ${path}
 cp -r ~/.config/nvim/ ${path}
 cp -r ~/.config/waybar ${path}
-cp -r ~/.config/yazi ${path}
 cp -r ~/.config/rofi ${path}
 
 # System config
